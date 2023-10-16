644877b5faba3ad3d2f567ea50ce4e5e7169ee1a
karl ducta <karlducta022@gmail.com>
2023-10-16 11:51:49 +0800

    --- Converting CommonJS to ES Module ---

--- notes app/app.js
+++ notes app/app.js
@@ -1,5 +1,7 @@
-const getNotes = require("./notes.js");
+import chalk from "chalk";
+
+import { getNotes } from "./notes.js";
 
 const msg = getNotes();
 
-console.log(msg);
+console.log(chalk.green(msg));
