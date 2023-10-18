47e236ff625e2a9f27f395e5ab49d87cc415e4bb
karl ducta <karlducta022@gmail.com>
2023-10-18 21:09:49 +0800

    --- Read JSON file --- Parse JSON ---

---
+++ notes app/playground/1-json.js
@@ -0,0 +1,14 @@
+import fs from "fs";
+
+// const book = {
+//   title: "Ego is the Enemy",
+//   author: "Ryan Holiday",
+// };
+
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync("1-json.json", bookJSON);
+
+const dataBuffer = fs.readFileSync("1-json.json");
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+console.log(data.title);
